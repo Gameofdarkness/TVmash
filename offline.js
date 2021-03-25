a143691c52a9b4eadfd51ad02890c04b3ea14b2a
Diyar_T <dyartymwry1@gmail.com>
2021-03-25 16:44:49 +0430

    TV Mash Application!

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1616674382,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"backgroung_blue.png",
+		"square-sheet0.png",
+		"square-sheet1.png",
+		"square-sheet2.png",
+		"logo-sheet0.png",
+		"buttons-sheet0.png",
+		"items-sheet0.png",
+		"pacman_icon-sheet0.png",
+		"pacman_icon-sheet1.png",
+		"pacman_icon-sheet2.png",
+		"sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
